7b2df5b89aae5f69804ace8afd6ac770dd880d24
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-03-26 05:10:34 +0530

    add

---
+++ order.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "LAVENDER KF102 Drone",
+    productNumber: "36378",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Ruko Fu Pro Drone",
+    productNumber: "56784",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone With Camera Drone",
+    productNumber: "34798",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "GPS 4k Drone",
+    productNumber: "99865",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "DJI Air 2S",
+    productNumber: "56335",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Lozenge Drone",
+    productNumber: "00481",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
